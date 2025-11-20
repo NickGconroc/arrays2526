02ed13dba0810a7c7406dce59894bbb9e59750d6
Rodrigo <235055043+NickGconroc@users.noreply.github.com>
2025-11-20 16:25:08 +0100

    ejerciciosdemoar finalizados

--- src/main/java/daw/MoarEjercicios.java
+++ src/main/java/daw/MoarEjercicios.java
@@ -5,93 +5,78 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MoarEjercicios {
+
+    private static Random ran = new Random(); // Mantengo el nombre del Random tal cual está
+    private static Scanner tec = new Scanner(System.in); // Mantengo el nombre del Scanner tal cual está
+
     public static void main(String[] args) {
+        // Ejercicio 1: Array de números aleatorios entre 1 y 5, y contar repeticiones
+        System.out.println("Ejercicio 1: Array de números aleatorios entre 1 y 5");
+        int maxValor = obtenerEntradaUsuario(); // Obtener el número que define el rango
+        int[] arrayNumerosAleatorios = generarArrayAleatorio(maxValor); // Generar el array con números aleatorios
+        contarRepeticiones(arrayNumerosAleatorios, maxValor); // Contar las repeticiones de cada número
 
-        int respuesta = preguntaUsuario();
-        int[] arrayFInal = arrayDel1al5(respuesta);
-        arrayConteoNumeros(arrayFInal);
+        System.out.println("\n--- Fin del Ejercicio 1 ---\n");
 
-        int [] arrayOriginal = {4,5,6,7,8};
+        // Ejercicio 2: Copiar un array e invertirlo
+        System.out.println("Ejercicio 2: Copiar e invertir un array");
+        int[] arrayOriginal = {4, 5, 6, 7, 8}; // Array original para copiar e invertir
+        int[] arrayCopia = copiarArray(arrayOriginal); // Copiar el array original
+        System.out.println("Array copiado: " + Arrays.toString(arrayCopia));
 
-        int[] arraycopiado = Copiar(arrayOriginal);
-        System.out.println(Arrays.toString(arraycopiado));
+        int[] arrayInvertido = invertirArray(arrayCopia); // Invertir el array copiado
+        System.out.println("Array invertido: " + Arrays.toString(arrayInvertido));
 
-        int[] inversoo = copiaInvertida(arraycopiado);
-        System.out.println(Arrays.toString(inversoo));
-
+        System.out.println("\n--- Fin del Ejercicio 2 ---\n");
     }
 
-    // Pedir al usuario meter en un array de 10 posiciones un numero del 1 al 5
-    // Otra funcion a partir del array anterior(parametro) que devuelva otro array
-    // contando los numeros que se repiten de cada uno
-
-    private static Random ran = new Random(); // La hacemos privada para usar solo en esta CLASE (public class)
-    private static Scanner tec = new Scanner(System.in);
-
-    public static int preguntaUsuario() {
-        int respuesta;
-        System.out.println("DIme un numero del 1 al 5 para rellenar el array");
-        respuesta = tec.nextInt();
-        return respuesta;
+    // Ejercicio 1: Preguntar al usuario un número entre 1 y 5
+    public static int obtenerEntradaUsuario() {
+        System.out.println("Dime un numero del 1 al 5 para rellenar el array");
+        return tec.nextInt(); // Leemos el valor que introduce el usuario
     }
 
-    // Creamos un array desde el 1 hasta al numero que diga el usuario, el array es
-    // del tamaño [10] ya predefinido por el ejercicio
-    public static int[] arrayDel1al5(int respuesta) {
-        int[] arrayDel1al5 = new int[10];
-        for (int i = 0; i < arrayDel1al5.length; i++) {
-            int numeroAñadir = ran.nextInt(1, respuesta);
-            arrayDel1al5[i] = numeroAñadir;
-            System.out.println(arrayDel1al5[i]);
-
+    // Ejercicio 1: Generar un array con números aleatorios entre 1 y el número dado por el usuario
+    public static int[] generarArrayAleatorio(int maxValor) {
+        int[] arrayAleatorio = new int[10]; // Creamos un array de 10 posiciones
+        for (int i = 0; i < arrayAleatorio.length; i++) {
+            int valorAleatorio = ran.nextInt(1, maxValor + 1); // Generamos un valor aleatorio entre 1 y 'maxValor'
+            arrayAleatorio[i] = valorAleatorio;
+            System.out.println(arrayAleatorio[i]); // Imprimimos el número generado
         }
-        return arrayDel1al5;
+        return arrayAleatorio;
     }
 
-    public static int[] arrayConteoNumeros(int[] arrayDel1al5) {
-        int[] arrayConteado = arrayDel1al5;
-        for (int i = 0; i < arrayDel1al5.length; i++) {
-            if (arrayDel1al5[i] == 1) {
-                arrayConteado[0]++;
-            }
-            if (arrayDel1al5[i] == 2) {
-                arrayConteado[1]++;
-            }
-            if (arrayDel1al5[i] == 3) {
-                arrayConteado[2]++;
-            }
-            if (arrayDel1al5[i] == 4) {
-                arrayConteado[3]++;
-            }
-            if (arrayDel1al5[i] == 4) {
-                arrayConteado[4]++;
-            }
-        }
-        System.out.println(Arrays.toString(arrayConteado));
-        return arrayConteado;
-    }
+    // Ejercicio 1: Contar las repeticiones de cada número en el array
+    public static void contarRepeticiones(int[] arrayAleatorio, int maxValor) {
+        int[] contadorRepeticiones = new int[maxValor]; // Array para contar cuántas veces aparece cada número
 
-    //Hacer un array de numeros o letras, hacer una funcion Copiar que reciba el array y que haga una copia del array original
-    //Hacer una funcion copiaINvertida()
-    
-    public static int[] Copiar(int[] aCOpiar){
-        int[] arrayCopiado = new int[aCOpiar.length];
-        for (int i = 0; i < aCOpiar.length; i++) {
-            arrayCopiado[i] = aCOpiar[i];
-        }
-        
-
-        return arrayCopiado;
-    }
-
-    public static int[] copiaInvertida(int[] copiado){
-        int[] inverso = new int[copiado.length];
-        int j = -1;
-        for (int i = 0; i < copiado.length; i++) {
-            copiado[j] = inverso[i];
-            
+        // Contamos cuántas veces se repite cada número en el array
+        for (int i = 0; i < arrayAleatorio.length; i++) {
+            contadorRepeticiones[arrayAleatorio[i] - 1]++; // Incrementamos el contador para el número encontrado
         }
 
-        return inverso;
+        // Imprimimos el conteo de cada número
+        for (int i = 0; i < contadorRepeticiones.length; i++) {
+            System.out.println("El número " + (i + 1) + " se repite " + contadorRepeticiones[i] + " veces.");
+        }
+    }
+
+    // Ejercicio 2: Copiar un array
+    public static int[] copiarArray(int[] arrayAcopiar) {
+        int[] arrayCopia = new int[arrayAcopiar.length]; // Creamos un nuevo array con el mismo tamaño
+        for (int i = 0; i < arrayAcopiar.length; i++) {
+            arrayCopia[i] = arrayAcopiar[i]; // Copiamos los valores del array original
+        }
+        return arrayCopia; // Devolvemos el array copiado
+    }
+
+    // Ejercicio 2: Invertir un array
+    public static int[] invertirArray(int[] arrayAinvertir) {
+        int[] arrayInvertido = new int[arrayAinvertir.length]; // Creamos un nuevo array para el array invertido
+        for (int i = 0; i < arrayAinvertir.length; i++) {
+            arrayInvertido[i] = arrayAinvertir[arrayAinvertir.length - 1 - i]; // Asignamos los elementos en orden invertido
+        }
+        return arrayInvertido; // Devolvemos el array invertido
     }
 }
